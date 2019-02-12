322ea61c3b010f7f7d3ad3674553798bbb3e22ae
Erik EImterbäumer <erik@animarum.de>
2019-02-11 20:53:17 -0500

    listen port changed

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
   db: {
     uri: 'mongodb://root:root123@ds121455.mlab.com:21455/bootcampsoftwareengineering', //place the URI of your mongo database here.
   }, 
-  port: process.env.PORT || 3000
+  port: 8080
 };
